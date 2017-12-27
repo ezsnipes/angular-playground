36f5932f4b396db4ce1ebe1de7039a0070cb5840
Unknown <pddstudio.dev@gmail.com>
2017-12-27 09:10:06 +0100

    Root module updates

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import './polyfills';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.module';
+import "./polyfills";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-}).catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(ref => {
+    if (window["ngRef"]) {
+      window["ngRef"].destroy();
+    }
+    window["ngRef"] = ref;
+  })
+  .catch(err => console.error(err));
 
 /**
  * Copyright Google LLC All Rights Reserved.
